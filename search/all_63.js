8ca9d99b0b54e8a5abd8619d4fe44bb53be394b5
Glen Oakley <goakley123@gmail.com>
2013-04-13 21:26:43 +0000

    Incorporated Markdown into mainpage

--- search/all_63.js
+++ search/all_63.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['collec_2eh',['collec.h',['../collec_8h.html',1,'']]],
-  ['collec',['ColleC',['../index.html',1,'']]]
+  ['collec_2eh',['collec.h',['../collec_8h.html',1,'']]]
 ];
